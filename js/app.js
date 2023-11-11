719751810f1f71ac6cbfb31229a15c95bc2291ce
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-11 15:27:13 -0300

    finalizando projeto do carrinho

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let totalGeral 
+limpar();
+
 function adicionar(){
     //recuperar nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
@@ -8,29 +11,29 @@ function adicionar(){
     //Para pegar o valor, vamos usar a função split, mas separar pelo texto R$ e pegar o que vem após o R$, que é o valor
     let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
-    console.log(quantidade.value);
-    console.log(nomeProduto);
-    console.log(valorUnitario);
     let preco = quantidade.value * valorUnitario;
-    console.log(preco);
 
-    let lista = document.getElementById('lista-produtos');
-
-    //calcular o preço, o nosso subtotal
-    //adicionar no carrinho
-    //atualizar o valor total da compra
-    
-    if (quantidade.value.length == 0) {
-        window.alert('Informe a quantidade a ser adicionada');
+    if (quantidade.value.length > 0) {
+        //adicionando produto selecionado no carrinho
+        let carrinho = document.getElementById('lista-produtos');
+        carrinho.innerHTML = carrinho.innerHTML + 
+        `<section class="carrinho__produtos__produto">
+            <span class="texto-azul">${quantidade.value}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
+        //atualizar o valor total da compra
+        totalGeral += preco;
+        let campoTotal = document.getElementById('valor-total');
+        //como o campoTotal é uma tag de span, da pra mudar o texto dela com o comando textContent
+        campoTotal.textContent = `R$ ${totalGeral}`;
+        //limpando o campo de quantidade após apertar o botao adicionar
+        quantidade.value = '';
     } else {
-        console.log('------');
-        /*
-        console.log(quantidade.value);
-        console.log(nomeProduto);
-        console.log(valorUnitario);*/
+        window.alert('Informe a quantidade a ser adicionada');
     }
 }
 
 function limpar() {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 }
